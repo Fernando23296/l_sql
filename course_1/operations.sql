f8764da05fd44f6a5dbaae37db3bbf5813626612
fernando23296 <fernando23296@gmail.com>
2019-12-19 10:33:07 -0400

    sql files and more notes

---
+++ course_1/operations.sql
@@ -0,0 +1,6 @@
+INSERT INTO transactions(transaction_id, book_id, client_id, type, finished) VALUES(NULL, 3, 7, 'lend', 0);
+INSERT INTO transactions(transaction_id, book_id, client_id, type, finished) VALUES(NULL, 4, 8, 'sell', 0);
+INSERT INTO transactions(transaction_id, book_id, client_id, type, finished) VALUES(NULL, 6, 3, 'sell', 1);
+INSERT INTO transactions(transaction_id, book_id, client_id, type, finished) VALUES(NULL, 5, 2, 'lend', 1);
+INSERT INTO transactions(transaction_id, book_id, client_id, type, finished) VALUES(NULL, 10, 4, 'lend', 0);
+INSERT INTO transactions(transaction_id, book_id, client_id, type, finished) VALUES(NULL, 12, 9, 'sell', 1);
